69df350ba09bf670bd9434a995d130953e0fd822
simple-sifu <kattyntommy@gmail.com>
2023-04-13 12:13:35 -0400

    breaking change in React 18 !

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -9,3 +9,24 @@ export const Child = ({color}: ChildProps) => {
 export const ChildFC: React.FC<ChildProps> = (({color}) => {
     return <div>{color}</div>;
 })
+
+// ChildFC.displayName
+
+
+
+
+// In the upcoming lecture, we will be making use of the children prop in our Child and ChildAsFC components. You will eventually run into the following error:
+
+// TS2339: Property 'children' does not exist on type 'ChildProps'.
+
+// React 18 introduces a breaking change with the removal of implicit children in React.FunctionComponent types.
+
+// To fix this, we now must include the children in the interface along with our other props:
+
+
+interface ChildProps {
+    color: string;
+    onClick: () => void;
+    children?: React.ReactNode;
+  }
+
