4e6727f214ecca6aa61bf6c54fe2879c4af7d4a2
simple-sifu <kattyntommy@gmail.com>
2023-04-13 13:36:22 -0400

    adding 2nd prop onClick function to props definition

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,13 +1,26 @@
 interface ChildProps {
     color: string;
+    onClick: () => void;
 }
 
-export const Child = ({color}: ChildProps) => {
-    return <div>{color}</div>
+export const Child = ({color, onClick}: ChildProps) => {
+    return (
+        <div>
+            {color}
+            <button onClick={onClick}/>
+        </div>
+
+    )
+
 }
 
-export const ChildFC: React.FC<ChildProps> = (({color}) => {
-    return <div>{color}</div>;
+export const ChildFC: React.FC<ChildProps> = (({color, onClick}) => {
+    return (
+        <div>
+            {color}
+            <button onClick = {onClick} />
+        </div>
+    );
 })
 
 // ChildFC.displayName
