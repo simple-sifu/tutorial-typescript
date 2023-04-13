8e1f3580af985e23c7a8b57b282416c48629c603
simple-sifu <kattyntommy@gmail.com>
2023-04-13 18:03:07 -0400

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -2,41 +2,8 @@ import React,{FC} from 'react'
 import ReactDOM from 'react-dom/client'
 import UserSearch from './state/UserSearch'
 
-const Users = [
-  { 
-    name: 'Tommy',
-    age: 40
-  },
-  {
-    name: "Sarah",
-    age: 70
-  },
-  {
-    name: "Carol",
-    age: 30
-  }
-]
-
 const App = () => {
-
-  // const [name, setName] = useState('');
-  // const [user, setUser] = useState<{name: string, age: number} | undefined>();
-
-  // const clickHandler = () => {
-  //   const foundUser = Users.find(user => user.name.toLowerCase() === name.toLowerCase())
-  //   setUser(foundUser);
-  // }
-  // return (
-  //   <Fragment>
-  //       <UserSearch />
-
-  //       <input value={name} onChange={(e) => setName(e.target.value)}/>
-  //       <button onClick = {clickHandler}>Find User</button>
-  //       <div>{user && `name: ${user.name},  age: ${user.age}` }</div>
-  //   </Fragment>
-  // )
   return <UserSearch />
-
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
